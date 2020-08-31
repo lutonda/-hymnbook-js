556515032198deffc21a3a130631f57a6068dee3
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-08-31 18:24:59 +0100

    part create and  update fix

--- controllers/part.controller.js
+++ controllers/part.controller.js
@@ -4,39 +4,46 @@ let TypePart = require('../models/typePart')
 
 const { hashSync } = require('bcryptjs');
 
-exports.createOne = async(req, res) => {
-    
-        req.body.hymn = await Hymn.findById(req.body.hymn.id);
+exports.createOne = async (req, res) => {
 
-        let part = await Part.create(req.body);
-        
-        res.json({
-            status: 200,
-            message: "success",
-            data: part
-        });
+    req.body.hymn = await Hymn.findById(req.body.hymn.id);
+    req.body.typePart = await TypePart.findById(req.body.typePart.id);
+
+    let part = await Part.create(req.body);
+
+    res.json({
+        status: 200,
+        message: "success",
+        data: part
+    });
 }
 
-exports.updateOne = async(req, res) => {
+exports.updateOne = async (req, res) => {
 
 
-    let part = await Part.findById(req.params.id, async(err, data) => {
+    let part = await Part.findById(req.params.id, async (err, part) => {
 
-        part.text = req.body.text;
-        part.order = req.body.order;
-        part.typePart = await TypePart.findById(req.body.typePart.id);
+        if(req.body.text)
+            part.text = req.body.text;
+
+        if(req.body.order)
+            part.order = req.body.order;
+
+        if (req.body.typePart)
+            part.typePart = await TypePart.findById(req.body.typePart.id);
+
         part.save();
 
         res.json({
             status: 200,
             message: "success",
-            data: data || err
+            data: part || err
         })
     });
 
 }
 
-exports.deleteOne = async(req, res) => {
+exports.deleteOne = async (req, res) => {
     let part = await Part.findById(req.params.id, (err, data) => {
         part.remove();
 
@@ -51,7 +58,7 @@ exports.deleteOne = async(req, res) => {
 
 }
 
-exports.findOneBy = async(req, res) => {
+exports.findOneBy = async (req, res) => {
     let part = await Part.findById(req.params.id, (err, data) => {
         res.json({
             status: 200,
@@ -64,7 +71,7 @@ exports.findOneBy = async(req, res) => {
 
 }
 
-exports.findAllBy = async(req, res) => {
+exports.findAllBy = async (req, res) => {
     let parts = await Part.find({}).populate('hymn');
 
     res.json({
