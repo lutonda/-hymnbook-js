5b0e9564632647ee37a8144727926be6e4a34af0
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-09-04 15:03:03 +0100

    hymn create and seve controller

--- controllers/hymn.controller.js
+++ controllers/hymn.controller.js
@@ -5,48 +5,57 @@ let Language = require('../models/language');
 
 exports.createOne = async (req, res) => {
 
-    if (req.body.author.id)
+    if (req.body.author._id)
         req.body.author = await Author.findById(req.body.author.id);
     else {
-        req.body.author = await Author.create(req.body.author);
+        req.body.author = null//;await Author.create(req.body.author);
     }
 
     let parts = req.body.parts;
     req.body.parts = null;
 
-    let hymn = await Hymn.create(req.body);
+    req.body.language = await Language.findById(req.body.language._id)
 
-    parts.forEach(async (part) => {
-        part.hymn = hymn;
-        await Part.create(part)
+    Hymn.create(req.body, async (err, hymn) => {
+        parts.forEach(async (part) => {
+            part.hymn = hymn;
+            await Part.create(part)
+        });
+
+        res.json({
+            status: 200,
+            message: "success",
+            data: hymn
+        })
     });
 
-    res.json({
-        status: 200,
-        message: "success",
-        data: hymn
-    })
 }
 
 exports.updateOne = async (req, res) => {
 
     let hymn = await Hymn.findById(req.params.id, async (err, hymn) => {
 
-        if(req.body.number)
+        if (req.body.number)
             hymn.number = req.body.number;
-        if(req.body.title)
+        if (req.body.title)
             hymn.title = req.body.title;
         if (req.body.author)
             hymn.author = await Author.findById(req.body.author._id)
         if (req.body.language)
             hymn.language = await Language.findById(req.body.language._id)
 
-        req.body.parts.forEach(async part=>{
-            await Part.findById(part._id,async (err, newPart)=>{
-                newPart.text=part.text;
-                newPart.typePart=part.typePart;
-                newPart.save();
-            })
+        req.body.parts.forEach(async part => {
+            if (part._id)
+                await Part.findById(part._id, async (err, newPart) => {
+                    newPart.text = part.text;
+                    newPart.typePart = part.typePart;
+                    newPart.save();
+                })
+            else {
+                part.hymn = hymn;
+                await Part.create(part)
+            }
+
         })
         hymn.save();
 
