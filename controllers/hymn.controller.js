1ebba0612adf964c090028151e179dad05a81d88
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-09-08 16:41:58 +0100

    attach file to hymn

--- controllers/hymn.controller.js
+++ controllers/hymn.controller.js
@@ -69,16 +69,22 @@ exports.updateOne = async (req, res) => {
             var s = new Readable()
             s.push(imgBuffer)
             s.push(null)
-            const data = { name: hymn.title, data: s,type:file.type }
-             google.upload(data, (err, data) => {
-                 if(data){
-                     let file=new File();
-                     file._id=data.id;
-                     file.hymns.push(hymn)
-                     file.save
-                 }
-                 
-             })
+            const data = { name: hymn.number + '-' + hymn.title, data: s, type: file.type }
+            google.upload(data, async (err, data) => {
+                try {
+                    if (data) {
+                        let file = await File.create({ 'identity': data.id });
+                        file.identity = data.data.id
+                        file.hymns.push(hymn)
+                        file.save()
+                        hymn.files.push(file)
+                        hymn.save()
+                    }
+                } catch (e) {
+                    let y = e;
+                }
+
+            })
         })
 
         res.json({
@@ -103,14 +109,17 @@ exports.deleteOne = async (req, res) => {
 
 exports.findOneBy = async (req, res) => {
 
-    Hymn.findById(req.params.id, async (err, data) => {
-        data.parts = await Part.find({ "hymn": data.id }).populate('typePart');
+    Hymn.findById(req.params.id, async (err, hymn) => {
+        hymn.parts = await Part.find({ "hymn": hymn.id }).populate('typePart');
+        /* hymn.files.forEach(async file => {
+             file = await File.find(file._id)
+             })*/
         res.json({
             status: 200,
             message: "success",
-            data: data || err
+            data: hymn || err
         });
-    }).populate('author').populate('language');
+    }).populate('author').populate('language').populate('files');
 }
 
 exports.findAllBy = async (req, res) => {
